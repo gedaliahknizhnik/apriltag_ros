e47b45f70206bc686d6d3040e4b19a6de59cb4fb
Gedaliah Knizhnik <gknizhnik@roboticresearch.com>
2023-03-17 15:23:00 -0400

    Added python format launch file

---
+++ launch/v4l2_36h11.launch.py
@@ -0,0 +1,56 @@
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    device = 0
+
+    """Generate launch description with multiple components."""
+    container = ComposableNodeContainer(
+        name="apriltag_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="v4l2_camera",
+                plugin="v4l2_camera::V4L2Camera",
+                name="camera",
+                namespace="v4l2",
+                parameters=[{"video_device": f"/dev/video{device}"}],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+            ComposableNode(
+                package="image_proc",
+                plugin="image_proc::RectifyNode",
+                name="rectify",
+                namespace="v4l2",
+                remappings=[("/image", "/image_raw")],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+            # ComposableNode(
+            #     package="apriltag_ros",
+            #     plugin="AprilTagNode",
+            #     name="apriltag",
+            #     namespace="apriltag",
+            #     remappings=[
+            #         ("/apriltag/image_rect", "/v4l2/image_raw"),
+            #         ("/apriltag/camera_info", "/v4l2/camera_info"),
+            #     ],
+            #     parameters=[{"history": "keep_last"}],
+            #     extra_arguments=[{"use_intra_process_comms": True}],
+            # ),
+            # ComposableNode(
+            #     package="image_tools",
+            #     plugin="image_tools::ShowImage",
+            #     name="showimage",
+            #     remappings=[("/image", "/v4l2/image_raw")],
+            #     # parameters=[{"$(find-pkg-share apriltag_ros)/cfg/tags_36h11.yaml"}],
+            #     extra_arguments=[{"use_intra_process_comms": True}],
+            # ),
+        ],
+        output="both",
+    )
+
+    return launch.LaunchDescription([container])
